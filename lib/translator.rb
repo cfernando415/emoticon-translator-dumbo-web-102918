615142e4d520c88aa67d8ab0f338ce6f3770ec78
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-20 04:35:31 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
   result
 end
 
-def get_english_meaning(library, emoticon)
+def get_english_meaning(file_path, emoticon)
   # code goes here
   
 end
