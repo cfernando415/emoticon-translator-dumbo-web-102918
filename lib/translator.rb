2f7b0d8fde8fe3d1fd701ec00bfd01d2f1b2435b
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-20 04:04:01 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,17 @@
 # require modules here
+require "yaml"
 
 def load_library
   # code goes here
+  YAML.load_file('emoticons.yml')
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(library, emoticon)
   # code goes here
+  
 end
 
-def get_english_meaning
+def get_english_meaning(library, emoticon)
   # code goes here
+  
 end
